c7d99fc33004a9b199d05eb53dedd35a0a80c9fc
DwarkeshSojitra <135133031+DwarkeshSojitra@users.noreply.github.com>
2023-06-06 15:12:10 +0530

    Add files via upload

---
+++ day 10/Ternaty Operatry/Q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("enter a:");
+	scanf("%d",&a);
+	
+	printf("enter b:");
+	scanf("%d",&b);
+	
+	 a>b ? printf("a is max") : printf("b is max");
+	 
+	return 0;
+}
